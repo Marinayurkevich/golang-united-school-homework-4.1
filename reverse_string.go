5f52193b49aedcefd9302a4f61732edd6fe6e102
marina <roxy-lo@inbox.ru>
2022-06-01 15:40:43 +0300

    line1

--- reverse_string.go
+++ reverse_string.go
@@ -1,10 +1,10 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	str:=[]rune(input)
-	var output[]rune
-	for i:=len(str)-1;i>=0;i--{
-	output=append(output,str[i])
+	line:=[]rune(input)
+	var output []rune
+	for i:=len(line)-1;i>=0;i--{
+	output=append(output,line[i])
 	}
-	return output
+	return string(output)
 }
