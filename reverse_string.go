2ac4129592cd61600502694124197c31254a7777
marina <roxy-lo@inbox.ru>
2022-06-01 16:29:39 +0300

    r1

--- reverse_string.go
+++ reverse_string.go
@@ -1,13 +1,10 @@
 package reverse_string
-
+import "strings"
 func ReverseString(input string) (output string) {
 	runes:=[]rune(input)
 	var line_runes []rune
 	for i:=len(runes)-1;i>=0;i--{
 	line_runes=append(line_runes,runes[i])
+	}	
+	return string(line_runes)
 	}
-	for i,r:=range line_runes{
-	output=append(output,line_runes[i])
-	}
-	return output
-}
